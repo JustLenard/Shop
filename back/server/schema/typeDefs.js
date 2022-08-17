89b469c45dd65ea5ea936f54990b9c281c325d1f
JustLenard <justlenard.justme@gmail.com>
2022-08-17 20:17:34 +0300

    finishet the query

--- back/server/schema/typeDefs.js
+++ back/server/schema/typeDefs.js
@@ -1,15 +1,18 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-  type Product {
-    price: Int!
-    name: String!
-  }
+    type Product {
+        price: Int!
+        name: String!
+        currentcy: String!
+        description: String!
+        image: String!
+    }
 
-  # Query
-  type Query {
-    getAllProducts: [Product!]!
-  }
-`;
+    # Query
+    type Query {
+        getAllProducts: [Product!]!
+    }
+`
 
-module.exports = { typeDefs };
+module.exports = { typeDefs }
