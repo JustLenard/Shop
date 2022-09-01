25aa95b5c189de96a0295a45fb7dcafd19c828bc
JustLenard <justlenard.justme@gmail.com>
2022-09-01 09:18:31 +0300

    a select is better

--- front/src/components/dropdowns/moneyDropdown/MoneyDropdown.tsx
+++ front/src/components/dropdowns/moneyDropdown/MoneyDropdown.tsx
@@ -4,11 +4,30 @@ import * as S from './MoneyDropdown.styles'
 interface Props {}
 
 const MoneyDropdown: React.FC<Props> = () => {
+    const money = [
+        {
+            currency: '$',
+            name: 'USD',
+        },
+        {
+            currency: 'Eur',
+            name: 'euro',
+        },
+    ]
     return (
-        <S.Wrapper>
-            <S.CurrentCurrency>asdf</S.CurrentCurrency>
-            <S.Dropdown></S.Dropdown>
-        </S.Wrapper>
+        // <S.Wrapper>
+        //     <S.CurrentCurrency></S.CurrentCurrency>
+        //     <S.Dropdown></S.Dropdown>
+        // </S.Wrapper>
+        <select>
+            {/* <option>Fuck</option> */}
+            {money.map((item, index) => (
+                <option>
+                    {item.currency}
+                    {item.name}
+                </option>
+            ))}
+        </select>
     )
 }
 
