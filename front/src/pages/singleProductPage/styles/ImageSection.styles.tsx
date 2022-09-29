2fcd42fe787ddacaf713fdc626f6f909b31dd905
JustLenard <justlenard.justme@gmail.com>
2022-09-29 18:11:16 +0300

    did some work on the images

--- front/src/pages/singleProductPage/styles/ImageSection.styles.tsx
+++ front/src/pages/singleProductPage/styles/ImageSection.styles.tsx
@@ -4,31 +4,18 @@ export { ProductName } from '../../../components/cards/styles/CartItemCard.style
 
 export const ImagesContainer = styled.div`
 	overflow: hidden;
-
-	border: 2px solid red;
-	/* 
-	@media ${size.md} {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	} */
 `
 
 export const Scroll = styled.div`
 	display: flex;
 
-	background-color: green;
-
 	justify-content: center;
-
-	border: 2px solid blue;
 `
 
 export const SvgWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	cursor: pointer;
-	background: transparent;
 
 	svg {
 		transition: all 150ms ease-in;
@@ -51,8 +38,6 @@ export const SmallImages = styled.div`
 
 	overflow-x: hidden;
 	scroll-behavior: smooth;
-
-	border: 2px solid green;
 `
 
 export const SmallImage = styled.img<{ highlight: boolean }>`
@@ -74,8 +59,6 @@ export const BigImageWrapper = styled.div`
 	max-height: 600px;
 
 	margin: 1rem auto;
-
-	border: 2px solid green;
 
 	img {
 		width: 100%;
